10e7293442558a9573ef6a1175e63f38741ca7da
yangpanpan <8300496+yang-panpanpan@user.noreply.gitee.com>
2021-01-14 11:46:35 +0800

    首次提交测试

---
+++ src/views/home/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+        首页
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {};
+    },
+    mounted() {},
+    methods: {},
+};
+</script>
+<style lang="scss" scoped>
+.home{
+    width: 100%;
+    height: 100%;
+    font-size: 0.24rem;
+}
+</style>
