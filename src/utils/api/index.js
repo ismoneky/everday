03a1d0a08bf2b5c0a006b765b22cc2d9df8246be
yangpanpan <8300496+yang-panpanpan@user.noreply.gitee.com>
2021-01-16 21:24:53 +0800

    首页逻辑

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,17 +1,31 @@
 import server from '../server/http'
 
-const getbanner = async function(){
-    var {data} = await server.get('/banner')
-    return data
+// banner轮播图
+const getbanner = async function () {
+    var {
+        data
+    } = await server.get('/banner')
+    return Promise.resolve(data)
 }
 
-const  getHome = async function(){
-    var {data} = await server.get('/recommend/appIndex')
-    return data
+// 首页列表
+const getHome = async function () {
+    var {
+        data
+    } = await server.get('/recommend/appIndex')
+    return Promise.resolve(data)
 }
 
+// 老师详情
+const getTeaDetail = async function (id) {
+    var {
+        data
+    } = await server.get(`/teacher/info/id=${id}`)
+    return Promise.resolve(data)
+}
 
 export {
-    getbanner,
-    getHome
+    getbanner, //轮播图
+    getHome, //首页列表
+    getTeaDetail, //老师详情
 }
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,20 +4,6 @@
     <swiper></swiper>
     <!-- 种类组件 -->
     <kinds></kinds>
-    <!-- 标题组件 -->
-    <titles title="名师阵容"></titles>
-    <!-- 讲师组件 -->
-    <teacher></teacher>
-    <!-- 标题组件 -->
-    <titles title="精品课程"></titles>
-    <!-- 课程组件 -->
-    <course></course>
-    <!-- 标题组件 -->
-    <titles title="推荐课程"></titles>
-    <!-- 课程组件 -->
-    <course></course>
-    <!-- 标题组件 -->
-    <titles title="明星讲师"></titles>
     <!-- 讲师组件 -->
     <teacher></teacher>
     <!-- 弹窗 -->
@@ -38,26 +24,38 @@
   </div>
 </template>
 <script>
+import { getHome } from "../../utils/api";
 import swiper from "../../components/home/swiper";
 import kinds from "../../components/home/kinds";
 import teacher from "@/components/home/teacher";
-import titles from "../../components/home/title";
-import course from "@/components/home/course";
+
 export default {
   components: {
     swiper,
     kinds,
     teacher,
-    titles,
-    course,
   },
   data() {
-    return {};
+    return {
+      list1: [],
+      list2: [],
+      list3: [],
+      list4: [],
+      list: [],
+      obj: {
+        name: "小黑",
+      },
+    };
   },
-  mounted() {
+  created() {
     this.yingcang();
   },
   methods: {
+    async fn() {
+      let { data } = await getHome();
+      console.log(data);
+      this.list = data;
+    },
     yingcang() {
       let show = "false";
       this.$store.commit("teashow", show);
